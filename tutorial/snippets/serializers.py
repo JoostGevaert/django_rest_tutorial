af0ea0277159b1656ba3eebf7655df83df4a2d39
Joost Gevaert <jhngevaert@gmail.com>
2020-12-29 19:37:28 +0100

    Get this repo from work laptop to my github

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -24,20 +24,20 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
 
 # Tutorial 2
-class SnippetSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-
-    class Meta:
-        model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
-
-
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'snippets']
+# class SnippetSerializer(serializers.ModelSerializer):
+#     owner = serializers.ReadOnlyField(source='owner.username')
+#
+#     class Meta:
+#         model = Snippet
+#         fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+#
+#
+# class UserSerializer(serializers.ModelSerializer):
+#     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+#
+#     class Meta:
+#         model = User
+#         fields = ['id', 'username', 'snippets']
 
 
 # class SnippetSerializer(serializers.Serializer):
